08439af0e31c38313074f0dd89fc243fb8f6c1f1
NyeongC <cjfsud23@naver.com>
2024-01-28 16:56:32 +0900

    전개 연산자

---
+++ 원쌤의 Vue.js 퀵스타트/test/build/02-23.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var arr1 = [1, 2, 3];
+var arr2 = ["hello"].concat(arr1, ["world"]);
+console.log(arr1);
+console.log(arr2);
---
+++ 원쌤의 Vue.js 퀵스타트/test/src/02-23.js
@@ -0,0 +1,4 @@
+let arr1 = [1,2,3,];
+let arr2 = ["hello", ...arr1, "world"];
+console.log(arr1);
+console.log(arr2);
