0d5834229dd802c983ea9c4a013ec9f3e51616e9
NyeongC <cjfsud23@naver.com>
2024-02-21 22:44:37 +0900

    전역 스타일 순서

---
+++ 원쌤의 Vue.js 퀵스타트/style-test/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <Child1></Child1>
+  <Child2/>
+  <Child3/>
+  
+</template>
+
+<script>
+import Child1 from './components/Child1.vue'
+import Child2 from './components/Child2.vue'
+import Child3 from './components/Child3.vue'
+export default{
+  name :"App",
+  components :{Child1, Child2, Child3}
+}
+/*
+자식 컴포넌트가 서로 다른 스타일을 가지지만
+모두 전역 클래스이기에 충돌
+1,2,3 순으로 임포트 되었지만
+스타일은 마지막에 임포트된 컴포넌트의 스타일 속성으로 적용됨
+따라서 3번째 orange가 적용
+*/
+</script>
+
---
+++ 원쌤의 Vue.js 퀵스타트/style-test/src/components/Child3.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="child">
+        <h2>Child3</h2>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name:"child3",
+
+}
+
+</script>
+
+<style>
+.child {background-color: orange; border: solid 1px black; margin:1.5em; padding: 1.0em;}
+</style>
